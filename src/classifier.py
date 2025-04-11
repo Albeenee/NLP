2e1793501107a80f8618968a6377db3246411496
Albane Vigier <albanevigier@MacBook-Pro-de-Albane.local>
2025-04-11 12:48:38 +0200

    fix device pb

--- src/classifier.py
+++ src/classifier.py
@@ -1,6 +1,19 @@
 from typing import List
 
 import torch
+from torch.utils.data import DataLoader
+import pandas as pd
+from datasets import Dataset
+from transformers import RobertaTokenizer, TrainingArguments, Trainer, DataCollatorWithPadding
+
+import numpy as np
+import os
+
+from transformers import RobertaConfig
+
+from create_dataset import create_dataset
+from model import RobertaForWeightedClassification
+from compute_metrics import compute_metrics
 
 
 class Classifier:
@@ -9,19 +22,30 @@ class Classifier:
     the 2 methods train() and predict() below. Please do not change the signature of these methods
      """
 
-
     ############################################# complete the classifier class below
     
     def __init__(self, ollama_url: str):
         """
-        This should create and initilize the model.
+        This should create and initialize the model.
         !!!!! If the approach you have choosen is in-context-learning with an LLM from Ollama, you should initialize
          the ollama client here using the 'ollama_url' that is provided (please do not use your own ollama
          URL!)
         !!!!! If you have choosen an approach based on training an MLM or a generative LM, then your model should
         be defined and initialized here.
         """
-        pass
+
+        HF_TOKEN = 'XXX'
+        os.environ["WANDB_MODE"] = "disabled"
+
+        self.tokenizer = RobertaTokenizer.from_pretrained("roberta-base", token=HF_TOKEN)
+        self.model = None  # Initialized during train()
+        self.trainer = None
+        self.label2id = {}
+        self.id2label = {}
+
+        os.environ["WANDB_MODE"] = "disabled"
+        self.device = torch.device("cpu") # default
+        
 
     
     
@@ -39,6 +63,65 @@ class Classifier:
 
         """
 
+        self.device = device
+        HF_TOKEN= 'XXX'
+
+        # Load the dataset
+        df = pd.read_csv(train_filename, delimiter='\t', on_bad_lines='skip',
+                         header=None, names=['label', 'catégorie', 'heure', 'origin', 'texte'])
+
+
+
+        # PREPROCESSING DF
+        df['input'] = df['catégorie'].astype(str) + ' : ' + df['texte'].astype(str)
+        df = df.dropna(subset=['input', 'label'])
+
+        # Encoding of the labels
+        labels = df['label'].unique()
+        self.label2id = {label: i for i, label in enumerate(labels)}
+        self.id2label = {i: label for label, i in self.label2id.items()}
+        df['label'] = df['label'].map(self.label2id)
+
+        # Create datasets
+        train_ds, test_ds, data_collator, class_weights_tensor = create_dataset(df, labels, HF_TOKEN)
+
+
+        # Model and tokenizer
+        config = RobertaConfig.from_pretrained("roberta-base", num_labels=len(labels))
+
+        self.model = RobertaForWeightedClassification.from_pretrained(
+            "roberta-base",
+            config=config,
+            class_weights=class_weights_tensor
+        ).to(device)
+
+        training_args = TrainingArguments(
+            output_dir="./results",
+            eval_strategy="epoch",
+            learning_rate=2e-5,
+            per_device_train_batch_size=8,
+            per_device_eval_batch_size=8,
+            num_train_epochs=3,
+            weight_decay=0.01,
+            logging_dir="./logs",
+            logging_steps=10,
+            save_strategy="no",
+            report_to=None,
+            remove_unused_columns=False
+        )
+
+        data_collator = DataCollatorWithPadding(tokenizer=self.tokenizer)
+
+        self.trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=train_ds,
+            eval_dataset=test_ds,
+            compute_metrics=compute_metrics,
+            data_collator=data_collator,
+        )
+
+        self.trainer.train()
 
 
     def predict(self, data_filename: str, device: torch.device) -> List[str]:
@@ -52,8 +135,34 @@ class Classifier:
           - PUT THE MODEL and DATA on the specified device! Do not use another device
         """
 
+        df = pd.read_csv(data_filename, delimiter='\t', on_bad_lines='skip',
+                     header=None, names=['label', 'catégorie', 'heure', 'origin', 'texte'])
+    
+        df['input'] = df['catégorie'].astype(str) + ' : ' + df['texte'].astype(str)
+        df = df.dropna(subset=['input'])
 
+        pred_ds = Dataset.from_pandas(df[['input']])
 
+        def tokenize_function(example):
+            return self.tokenizer(example["input"], padding="max_length", truncation=True)
 
+        pred_ds = pred_ds.map(tokenize_function, batched=False)
+        pred_ds = pred_ds.remove_columns([col for col in pred_ds.column_names if col not in ['input_ids', 'attention_mask']])
+        pred_ds.set_format("torch")
 
+        self.model.to(device)
+        self.model.eval()
 
+        dataloader = DataLoader(pred_ds, batch_size=8)
+
+        all_preds = []
+
+        with torch.no_grad():
+            for batch in dataloader:
+                batch = {k: v.to(device) for k, v in batch.items()}
+                outputs = self.model(**batch)
+                logits = outputs.logits
+                preds = torch.argmax(logits, dim=-1)
+                all_preds.extend(preds.cpu().numpy())
+
+        return [self.id2label[i] for i in all_preds]
